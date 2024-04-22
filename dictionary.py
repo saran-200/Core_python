4d1e5d4d343246133e59f0bd4e423dba58878f6d
saran-200 <saran.r.it.mec@gmail.com>
2024-04-22 14:45:03 +0530

    modified:   dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,13 +1,31 @@
-a={"name":"saran","age":19,"clg":"MEC"}
-a["PHONE NO"]=9344430126
-a.update({"place":"salem"})
-print(a)
-print(a["clg"])
-print(a.items())
-print(a.values())
-print(a.keys())
-print(a.get("age"))
-b=a.copy()
-print(b)
-c=b.clear()
-print(c)
+#a={"name":"saran","age":19,"clg":"MEC"}
+# a["PHONE NO"]=9344430126
+# a.update({"place":"salem"})
+# print(a)
+# print(a["clg"])
+# print(a.items())
+# print(a.values())
+# print(a.keys())
+# print(a.get("age"))
+# b=a.copy()
+# print(b)
+# c=b.clear()
+# print(c)
+# a.setdefault("degree","BE")
+# print(a)
+#a={"name":"saran","age":19,"clg":"MEC","degree":"BE","phno":12345346}
+# for i in a:
+#     print(i,":",a[i])
+# for i in a.values():
+#     print(i)
+# # for i,j in a.items():
+# #     print(i,j)
+# print(a.pop("name"))
+# print(a)
+# a.setdefault("name","saran")
+# print(a)
+# del a["age"]
+# print(a)
+
+
+
