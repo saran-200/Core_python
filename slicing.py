e8460893e7f0bf256263ff999faf74509b19dd04
saran-200 <saran.r.it.mec@gmail.com>
2024-04-22 12:23:35 +0530

    modified:   slicing.py

--- slicing.py
+++ slicing.py
@@ -1,4 +1,18 @@
-a=[1,3,45,'ramanas',56]
-print(a[0:4])
-print(a[2:])
-print(a[:4])
+# a=[1,3,45,'ramanas',56]
+# print(a[0:4])
+# print(a[2:])
+# print(a[:4])
+# a=(56,'r7',234,'rty')
+# print(type(a))
+# b=(1,23,23,23)
+# print(b)
+# a=(12,34,56,65,34)
+# print(a)
+# b=list(a)
+# b.append('saran')
+# a=tuple(b)
+# print(a)
+a=(12,34,45,234,345,)
+b=(1,)
+a+=b
+print(a)
