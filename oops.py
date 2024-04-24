43e5c0382109119571d7ee12b6eb5f5d7ea89242
saran-200 <saran.r.it.mec@gmail.com>
2024-04-24 13:27:26 +0530

    modified:   oops.py

--- oops.py
+++ oops.py
@@ -1,8 +1,36 @@
-class college:
-    def __init__(self,name,course):
-        self.n=name
-        self.c=course
-    def print(self):
-        print(self.n,self.c)
-c=college("saran","IT")
-c.print()
+# class college:
+#     def __init__(self,name,course):
+#         self.n=name
+#         self.c=course
+#     def print(self):
+#         print(self.n,self.c)
+# c=college("saran","IT")
+# c.print()
+
+# ======> single
+# class bike:
+#     def gear(self):
+#         print("speed")
+# class car(bike):
+#     def ride(self):
+#         print("race")
+# c=car()
+# c.gear()
+# c.ride()
+# =======> multilevel
+# class college:
+#     def clg (self):
+#         print("MEC")
+# class dept(college):
+#     def dep(self):
+#         print("IT")
+# class sect(dept):
+#     def sec(self):
+#         print("no section")
+# a=sect()
+# a.clg()
+# a.dep()
+# a.sec()
+
+
+        
