a2c24085da0b99352942f2e289baf6e6a95f4e45
saran-200 <saran.r.it.mec@gmail.com>
2024-04-26 14:35:13 +0530

    new file:   overriding.py

---
+++ overriding.py
@@ -0,0 +1,13 @@
+class sam:
+    def name(self):
+        print("sam")
+class raja(sam):
+    def name(self):
+        super().name()
+        print("raja")
+class arun(raja):
+    def name(self):
+        super().name()
+        print("ARUN")
+s=arun()
+s.name()
